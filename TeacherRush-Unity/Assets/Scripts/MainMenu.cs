99ae4cb41356cb00f8283c0dd102493ac6b407f8
KondertTim <KondertTim@users.noreply.github.com>
2020-05-03 20:47:15 +0200

    Menus

---
+++ TeacherRush-Unity/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void pressedPlay()
+   {
+      SceneManager.LoadScene("SampleScene");
+   }
+
+   public void pressedOptions()
+   {
+      SceneManager.LoadScene("Options");
+
+   }
+
+   public void pressedExit()
+   {
+      Application.Quit();
+   }
+}
---
+++ TeacherRush-Unity/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isGamePaused = false;
+    public GameObject pauseMenuUI;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isGamePaused)
+            {
+                resume();
+            }
+            else
+            {
+                pause();
+            }
+        }
+    }
+
+    public void resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isGamePaused = false;
+    }
+
+    private void pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isGamePaused = true;
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+    public void goToMenu()
+    {
+        SceneManager.LoadScene("MainMenue");
+    }
+    
+}
